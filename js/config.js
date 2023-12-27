ebd26a2c31cc2c46db3a6f7390e2d00a91497a72
Vladmidir <vladgoran2004@gmail.com>
2023-12-27 18:39:27 -0500

    config cognito added

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-2_KJwy7YWOF', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5pvjgo8u7p4ir0o956d290dm37', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
